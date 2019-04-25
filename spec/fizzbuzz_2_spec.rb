bf879cda2e8af3c6de1e69cdc082bfceb8cd6743
EC2 Default User <ec2-user@ip-172-31-11-44.us-east-2.compute.internal>
2019-04-25 12:34:17 +0000

    works for numbers not a multiple of 3 or 5

--- spec/fizzbuzz_2_spec.rb
+++ spec/fizzbuzz_2_spec.rb
@@ -20,5 +20,8 @@ describe 'fizzbuzz_2' do
   it 'returns "fizzbuzz" for multiples of 3 and 5' do
     expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
-
+  
+  it 'returns number for all numbers not a multiple of 3 or 5' do
+    expect(1.fizzbuzz).to eq 1
+  end
 end
