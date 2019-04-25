325152b0ac7f2b62c43b2136c1fd243f6731028c
EC2 Default User <ec2-user@ip-172-31-11-44.us-east-2.compute.internal>
2019-04-25 12:37:22 +0000

    refactors

--- lib/fizzbuzz_2.rb
+++ lib/fizzbuzz_2.rb
@@ -1,13 +1,22 @@
 class Integer
   def fizzbuzz
-    if self % 3 == 0 && self % 5 == 0
+    if multiple_of_3 && multiple_of_5
       'fizzbuzz'
-    elsif self % 3 == 0
+    elsif multiple_of_3
       'fizz'
-    elsif self % 5 == 0
+    elsif multiple_of_5
       'buzz'
     else
       self
     end
   end
+  
+  def multiple_of_3
+    self % 3 == 0
+  end
+  
+  def multiple_of_5
+    self % 5 == 0
+  end
 end
+
