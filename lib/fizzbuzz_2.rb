43b90dbad7674d9324f027c6af7acdfc1c021d02
EC2 Default User <ec2-user@ip-172-31-11-44.us-east-2.compute.internal>
2019-04-25 12:22:20 +0000

    works for 3 and 5

---
+++ lib/fizzbuzz_2.rb
@@ -0,0 +1,9 @@
+class Integer
+  def fizzbuzz
+    if self == 3
+      'fizz'
+    elsif self == 5
+      'buzz'
+    end
+  end
+end
---
+++ spec/fizzbuzz_2_spec.rb
@@ -0,0 +1,11 @@
+require 'fizzbuzz_2'
+
+describe 'fizzbuzz_2' do
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+  
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+end
